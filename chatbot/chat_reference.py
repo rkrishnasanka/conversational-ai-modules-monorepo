2bcaa14d2785cce2cbcf653916a3629dcea46743
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-09-04 21:11:36 +0000

    - Created a dataclass for packaging and handling the references. - Cleaned up unnessary client call in `get_conversation()` - Updated the prompt generation to use a method that loads up the message history and stuff - Updated the chromadb connection to use the remote server - Updated the converse method to have all the updated features.

---
+++ chatbot/chat_reference.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ChatReference:
+    """Chat Reference Dataclass"""
+    title: str
+    description: str
+    ref_url: str
+    context: str
