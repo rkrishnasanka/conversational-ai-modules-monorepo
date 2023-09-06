d95a63d72b2d9bac8a1d2c772e04c0a97313242e
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-09-06 20:28:18 +0000

    Ran black to format everything

--- chatbot/chat_reference.py
+++ chatbot/chat_reference.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class ChatReference:
     """Chat Reference Dataclass"""
+
     title: str
     description: str
     ref_url: str
--- scripts/content_helper.py
+++ scripts/content_helper.py
@@ -1,11 +1,9 @@
-
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import OpenAI
 from langchain.chains import VectorDBQA
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
 
 OPEN_API_KEY = "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"
 
@@ -18,9 +16,11 @@ texts = text_splitter.split_documents(documents)
 
 # Embed and store the texts
 # Supplying a persist_directory will store the embeddings on disk
-PERSIST_DIRECTORY = '../../content/db'
+PERSIST_DIRECTORY = "../../content/db"
 
 embedding = OpenAIEmbeddings(openai_api_key=OPEN_API_KEY)
-vectordb = Chroma.from_documents(documents=texts, embedding=embedding, persist_directory=PERSIST_DIRECTORY)
+vectordb = Chroma.from_documents(
+    documents=texts, embedding=embedding, persist_directory=PERSIST_DIRECTORY
+)
 
 vectordb.persist()
